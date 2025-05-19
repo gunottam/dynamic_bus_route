b40c4d3406ded29112e9cfb886962c3a6975ac7a
Gunottam Maini <gunottammaini@gmail.com>
2025-05-19 21:34:07 +0530

    update

--- bus-route-system/src/maps/folium_utils.py
+++
@@ -1,6 +0,0 @@
-def create_map(location, zoom_start=13):
-    import folium
-    return folium.Map(location=location, zoom_start=zoom_start)
-
-def add_route_to_map(map_obj, route, color='blue'):
-    folium.PolyLine(locations=route, color=color, weight=5).add_to(map_obj)
